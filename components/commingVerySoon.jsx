fcbcc4a2515a52a696ba23dd6d3399ddb7a78028
love-to-code108 <love.to.code108@gmail.com>
2023-08-20 16:50:01 +0530

    the coming soon page is done

--- components/commingVerySoon.jsx
+++ components/commingVerySoon.jsx
@@ -5,11 +5,31 @@ export const CommingSoon = () => {
 
 
 
-    return(
+    return (
         <div id="ComingSoon" className="
-        ">
+        flex flex-col justify-center items-center">
+
+            
+            <div>
+                <p className="
+            text-white text-6xl font-netflix2
+            md:text-8xl">SOMETHING'S</p>
+            </div>
+           
+
+
             <p className="
-            text-white text-7xl font-netflix2">COMING SOON</p>
+            text-[#E50914] text-4xl md:text-6xl font-netflix2
+            bg-white pt-1 px-2">COMING VERY SOON</p>
+
+
+            <br />
+            <br />
+            <br />
+
+            <p className="text-white fixed ready font-netflix
+            lg:text-2xl">I HOPE YOUR ARE <span className=" text-[#E50914] ">READY</span> FOR IT</p>
+
         </div>
     );
 }
