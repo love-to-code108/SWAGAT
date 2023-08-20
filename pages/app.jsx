3775fc5f4a497a921ff847ee8f431ae3290e8983
love-to-code108 <love.to.code108@gmail.com>
2023-08-20 17:33:01 +0530

    done the coming soon page

--- pages/app.jsx
+++ pages/app.jsx
@@ -1,7 +1,7 @@
 // IMPORTING OTHER LIBRARIES BELOW
-import { LandingPage } from "../components/LandingPage";
+// import { LandingPage } from "../components/LandingPage";
 import { CommingSoon } from "../components/commingVerySoon";
-import { LandingAnimation } from "../components/landingAnimation";
+// import { LandingAnimation } from "../components/landingAnimation";
 
 
 // IMPORTING CSS BELOW
