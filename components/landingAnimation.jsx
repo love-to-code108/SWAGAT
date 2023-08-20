c9321354a8dba447ae7456d7cb8dfa257c0eba30
love-to-code108 <love.to.code108@gmail.com>
2023-08-20 12:19:40 +0530

    currently trying to add the landing animation

--- components/landingAnimation.jsx
+++ components/landingAnimation.jsx
@@ -1,12 +1,33 @@
 import "../src/index.css"
 
+// IMPORTING ASSETS BELOW
+import landingVideo from "../src/assets/videos/Netflix New Logo Animation 2019.mp4"
+import landingAudio from "../src/assets/audio/Netflix New Logo Animation 2019.mp3"
+
 export const LandingAnimation = () => {
 
 
 
-    return(
-        <div>
-            Hello world
+    return (
+        <div id="LandingAnimation" className=" flex justify-center items-center">
+
+            <div>
+
+
+
+                <audio autoPlay loop >
+                    <source src={landingAudio} />
+                </audio>
+
+
+                <video autoPlay muted loop className="LandingAnimationSrc">
+                    <source src={landingVideo} />
+                </video>
+
+
+
+            </div>
+
         </div>
     );
 }
