f273da615ac92d515b346bde38640e1cd51bdb7a
love-to-code108 <love.to.code108@gmail.com>
2023-08-20 17:20:53 +0530

    the coming soon page is done

--- components/commingVerySoon.jsx
+++ components/commingVerySoon.jsx
@@ -9,18 +9,20 @@ export const CommingSoon = () => {
         <div id="ComingSoon" className="
         flex flex-col justify-center items-center">
 
-            
-            <div>
-                <p className="
+
+            <div id="Somethings">
+                <p id="SomethingsPara" className="
             text-white text-6xl font-netflix2
             md:text-8xl">SOMETHING'S</p>
             </div>
-           
 
 
-            <p className="
+            <div id="comingSoon">
+                <p id="comingSoonPara" className="
             text-[#E50914] text-4xl md:text-6xl font-netflix2
             bg-white pt-1 px-2">COMING VERY SOON</p>
+
+            </div>
 
 
             <br />
