9e9634db3c57cdeadefbe65cd23b4fbbc1a2d149
love-to-code108 <love.to.code108@gmail.com>
2023-09-06 01:22:32 +0530

    saving the work done so far the important links section is more or less done

--- components/linkSection.jsx
+++ components/linkSection.jsx
@@ -4,6 +4,7 @@ import "./styles/linksSection.css"
 // IMPORTING IMAGES
 import swagatLogoSmall from "../src/assets/svg/swagatSmallLogo.svg"
 import SofSwagat from "../src/assets/svg/s.svg"
+import GeeksForGeeksLogo from "../src/assets/svg/geeksForGeeksLogo.svg"
 
 export const LinksSection = () => {
 
@@ -54,10 +55,10 @@ export const LinksSection = () => {
 
 
                     <div className="theWritingSectionInTheCards relative">
-                        <div className="font-netflix2 joinLinksSection">JOIN</div>
-                        <div className="font-wednesday
-                        groupLinksSectionWhatsapp" >WHATSAPP</div>
-                        <div className="font-netflix2 groupLinksSection">GROUP</div>
+                        <div className="font-netflix2 joinLinksSection2">FOLLOW US ON</div>
+                        <div className="font-lucifer
+                        groupLinksSectionWhatsapp2" >INSTAGRAM</div>
+
                     </div>
 
 
@@ -66,16 +67,83 @@ export const LinksSection = () => {
 
 
                 {/* JOIN LINKEDIN SECTION */}
-                <div className="scrollableElementsLinksSection mr-2">
+                <div className="scrollableElementsLinksSection mr-16">
 
                 </div>
-
-
-
 
             </div>
 
 
+
+
+
+
+
+
+
+            {/* FOR NOW THE SPONSOR SECTION GOES HERE */}
+            <div className=" px-8 py-8">
+                <p className="font-netflix text-3xl text-white">Sponsors</p>
+            </div>
+
+
+            {/* THE HORIZONTALLY SCROLLABLE LINKS */}
+
+            <div id="WrapperForScrollableElements" className=" ml-8">
+
+
+
+                {/* GEEKS FOR GEEKS SECTION  */}
+                <div className="scrollableElementsLinksSection2 mr-2">
+
+
+                    <div className="smallSwagatSLogo"><img src={SofSwagat} alt="" /></div>
+
+
+                    <div className="theWritingSectionInTheCards2 relative">
+                        <img src={GeeksForGeeksLogo} alt="" />
+                        <p className="font-poppins font-black text-2xl GeeksForGeeks">Geeks For Geeks</p>
+                    </div>
+
+
+                </div>
+            </div>
+
+
+
+
+
+            {/* FOR NOW THE SPONSOR SECTION GOES HERE */}
+            <div className=" px-8 py-8">
+                <p className="font-netflix text-3xl text-white">For More Info</p>
+            </div>
+
+
+            {/* THE HORIZONTALLY SCROLLABLE LINKS */}
+
+            <div id="WrapperForScrollableElements" className=" ml-8">
+
+
+
+                {/* GEEKS FOR GEEKS SECTION  */}
+                <div className="scrollableElementsLinksSection2 mr-2">
+
+
+                    <div className="smallSwagatSLogo3"><img src={SofSwagat} alt="" /></div>
+
+
+                    <div className="theWritingSectionInTheCards3 relative">
+                        <p className="font-netflix2 text-4xl text-white  drop-shadow-lg">DOWNLOAD</p>
+                        <p className="font-netflix2 text-4xl text-[#FF0000] drop-shadow-lg">PDF</p>
+                    </div>
+
+
+                </div>
+            </div>
+
+
+
         </div>
+
     );
 }
