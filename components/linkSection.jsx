73c5a96a633c0a3ea0708fd774f0fe1100df1f68
love-to-code108 <love.to.code108@gmail.com>
2023-09-05 21:41:39 +0530

    working on the sliding section of the links section

--- components/linkSection.jsx
+++ components/linkSection.jsx
@@ -3,6 +3,7 @@ import "./styles/linksSection.css"
 
 // IMPORTING IMAGES
 import swagatLogoSmall from "../src/assets/svg/swagatSmallLogo.svg"
+import SofSwagat from "../src/assets/svg/s.svg"
 
 export const LinksSection = () => {
 
@@ -18,6 +19,63 @@ export const LinksSection = () => {
             <div className=" px-8 py-8">
                 <p className="font-netflix text-3xl text-white">Some Important Links</p>
             </div>
+
+
+            {/* THE HORIZONTALLY SCROLLABLE LINKS */}
+
+            <div id="WrapperForScrollableElements" className=" ml-8">
+
+
+
+                {/* JOIN WHATSAPP SECTION  */}
+                <div className="scrollableElementsLinksSection mr-2">
+
+
+                    <div className="smallSwagatSLogo"><img src={SofSwagat} alt="" /></div>
+
+
+                    <div className="theWritingSectionInTheCards relative">
+                        <div className="font-netflix2 joinLinksSection">JOIN</div>
+                        <div className="font-wednesday
+                    groupLinksSectionWhatsapp" >WHATSAPP</div>
+                        <div className="font-netflix2 groupLinksSection">GROUP</div>
+                    </div>
+
+
+                </div>
+
+
+
+                {/* JOIN INSTAGRAM SECTION*/}
+                <div className="scrollableElementsLinksSection mr-2">
+
+
+                    <div className="smallSwagatSLogo"><img src={SofSwagat} alt="" /></div>
+
+
+                    <div className="theWritingSectionInTheCards relative">
+                        <div className="font-netflix2 joinLinksSection">JOIN</div>
+                        <div className="font-wednesday
+                        groupLinksSectionWhatsapp" >WHATSAPP</div>
+                        <div className="font-netflix2 groupLinksSection">GROUP</div>
+                    </div>
+
+
+                </div>
+
+
+
+                {/* JOIN LINKEDIN SECTION */}
+                <div className="scrollableElementsLinksSection mr-2">
+
+                </div>
+
+
+
+
+            </div>
+
+
         </div>
     );
 }
