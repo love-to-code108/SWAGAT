278c7be3888242ccf1fef7c78f040c05ccef5dae
love-to-code108 <love.to.code108@gmail.com>
2023-09-19 02:13:51 +0530

    21st september stuff is done

--- components/twentyFirstSeptember.jsx
+++ components/twentyFirstSeptember.jsx
@@ -1,4 +1,7 @@
 import "../src/index.css"
+import EventBox from "./childComponents/eventBox"
+import EventBox2 from "./childComponents/eventBox2"
+import EventBox3 from "./childComponents/eventBox3"
 import "./styles/twentyFirstSeptember.css"
 
 const TwentyFirstSeptemberEventsAndRules = () => {
@@ -19,7 +22,7 @@ const TwentyFirstSeptemberEventsAndRules = () => {
 
                 {/* 21st september */}
                 <p className="
-                 font-netflix text-xl font-black ml-4 relative top-2 text-[#FF0000]"
+                 font-netflix text-xl font-black relative top-2 text-[#FF0000]"
                 >21st
                     <span
                         className="
@@ -32,7 +35,7 @@ const TwentyFirstSeptemberEventsAndRules = () => {
 
                 {/* swagat ki shurwat */}
 
-                <div className="w-[100%] bg-black flex justify-center">
+                <div className="w-[100%] bg-black flex justify-start">
                     <p
                         className="
                         font-netflix2 font-normal text-5xl text-white
@@ -57,9 +60,9 @@ const TwentyFirstSeptemberEventsAndRules = () => {
 
             {/* events */}
 
-            <div className="w-[100%] bg-black flex justify-start">
+            <div className="w-[100%] bg-black flex justify-start mb-12">
 
-                <p className=" font-netflix2 bg-white text-[#FF0000] text-4xl pt-1 px-2 ml-3
+                <p className=" font-netflix2 bg-white text-[#FF0000] text-4xl pt-1 px-2
                 ">EVENTS</p>
 
             </div>
@@ -72,12 +75,21 @@ const TwentyFirstSeptemberEventsAndRules = () => {
 
 
             {/* first box for treasure hunt */}
+            <div>
+                <EventBox/>
+            </div>
 
 
             {/* Second box for UEM GOT TALENT */}
-
+            <div>
+                <EventBox2/>
+            </div>
 
             {/* Third box for Rab ne banadi jodi */}
+            <div>
+                <EventBox3/>
+            </div>
+
 
 
             {/* want to mr and mrs freshers */}
