d9605a8c496c432e3e041e74be9fc8c278cd3dd3
love-to-code108 <love.to.code108@gmail.com>
2023-09-20 23:09:47 +0530

    the work for the twenty second september has started

---
+++ components/twentySecondSeptember.jsx
@@ -0,0 +1,12 @@
+import "../src/index.css"
+import "./styles/twentyFirstSeptember.css"
+
+import oneMillionLogo from "../src/assets/png/oneMilionLogo.png"
+
+const TwentySecondSeptember = () => {
+  return (
+    <div>twentySecondSeptember</div>
+  )
+}
+
+export default TwentySecondSeptember
